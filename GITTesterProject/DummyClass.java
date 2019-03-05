b780c600caa40c3ce82425a070d94e64f6d9ca0d
Trad Anthony <anthony.trad@isae.edu.lb>
2019-03-05 23:51:02 +0300

    Test commit avec un petit projet

---
+++ GITTesterProject/DummyClass.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class DummyClass here.
+ * Tester push et pull sur github 
+ * @Anthony Trad (your name)
+ * @1.0 (a version number or a date)
+ */
+public class DummyClass
+{
+    private long x;
+
+    /**
+     * Constucteur faisant nimporte quoi
+     */
+    public DummyClass()
+    {
+        // initialise instance variables
+        x = 123;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * @param  y  a sample parameter for a method
+     * @return the sum of x and y as String**/
+    public String testMethod(int y)
+    {
+        return String.valueOf(x + y);
+    }
+}
